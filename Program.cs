bbc3f996cc16c354c8ef87f663dd4eccc119a025
Roneet <roneetparida@gmail.com>
2019-05-14 19:06:35 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("Enter Number 1 : ");
+            num1 = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter Number 2 : ");
+            num2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Total of " + num1 + " and " + num2 + " is " + (num1 + num2));
+        }
+    }
+}
