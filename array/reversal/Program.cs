47c08929bb01ad71382742f9c37d422ef1e84906
Roneet <roneetparida@gmail.com>
2019-05-14 19:32:23 -0400

    reversal

---
+++ array/reversal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace reversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            for (short i = 0; i < 5; i++)
+            {
+                Console.Write("Enter Number " + (i + 1) + " : ");
+                numbers[i] = Convert.ToInt16(Console.ReadLine());
+            }
+
+            Console.Write("Array in Reverse is ");
+
+            for (short i = 4; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
