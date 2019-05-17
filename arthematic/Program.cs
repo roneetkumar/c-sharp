aec702d27fe6adc4b31a4a86f25a14540b2466a0
Roneet <roneetparida@gmail.com>
2019-05-17 14:06:58 -0400

    more programs

---
+++ arthematic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace arthematic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte val1, val2;
+
+            System.Console.Write("Enter Your First Number Less Than 255 : ");
+            val1 = Convert.ToByte(System.Console.ReadLine());
+            System.Console.Write("Enter Your Second Number Less Than 255 : ");
+            val2 = Convert.ToByte(System.Console.ReadLine());
+
+            System.Console.WriteLine("\n==========Result of Simple Calculator==========\n");
+            System.Console.WriteLine("Addition: {0} + {1} = {2}", val1, val2, (float)val1 + val2);
+            System.Console.WriteLine("\n=========================\n");
+            System.Console.WriteLine("Substraction: {0} - {1} = {2}", val1, val2, (float)val1 - val2);
+            System.Console.WriteLine("\n=========================\n");
+            System.Console.WriteLine("Multiplication: {0} * {1} = {2}", val1, val2, (float)val1 * val2);
+            System.Console.WriteLine("\n===============================\n");
+            System.Console.WriteLine("Division: {0} / {1} = {2}", val1, val2, (float)val1 / val2);
+            System.Console.WriteLine("\n=========================\n");
+            System.Console.WriteLine("Press any Key to Quit");
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ tryCatch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace tryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "", collegeName = "", city = "", address = "";
+            byte age = 0;
+            short id = 0;
+            bool check = false;
+
+            System.Console.Write("Enter your ID : ");
+            id = Convert.ToInt16(Console.ReadLine());
+            System.Console.Write("Enter your name : ");
+            name = Console.ReadLine();
+
+            do
+            {
+                try
+                {
+                    System.Console.Write("Enter your age : ");
+                    age = Convert.ToByte(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    System.Console.WriteLine("Age Cannot Be Negative or Alphabets");
+                    check = true;
+                }
+            } while (age < 1 || !check);
+
+            System.Console.Write("Enter your College Name : ");
+            collegeName = Console.ReadLine();
+            System.Console.Write("Enter your City : ");
+            city = Console.ReadLine();
+            System.Console.Write("Enter your Address : ");
+            address = Console.ReadLine();
+
+            System.Console.WriteLine("Student Info : ");
+            System.Console.WriteLine("My Name is {0}, I am {1} Years Old. My Student id is {2}", name, age, id);
+            System.Console.WriteLine("{0}, {1}, {2}", collegeName, address, city);
+            System.Console.ReadKey();
+        }
+    }
+}
