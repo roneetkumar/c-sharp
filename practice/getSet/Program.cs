d6040265fb65fc792f91fdcc4f2fb3c0a9266624
Roneet Kumar <roneetparida@gmail.com>
2019-06-07 10:43:00 -0400

    basic update

---
+++ practice/getSet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace getSet
+{
+    class Person
+    {
+        private string name = "Roneet";
+        private int age = 22;
+
+        // access private fields using getter, setter
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+        // access private fields using methods
+        public void print(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            System.Console.WriteLine(this.name);
+            System.Console.WriteLine(this.age);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+
+            person.print("Vinay", 100);
+
+            person.Name = "Roop";
+            person.Age = 200;
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+
+        }
+    }
+}
--- simpleCalc/Program.cs
+++ simpleCalc/Program.cs
@@ -4,10 +4,13 @@ namespace simpleCalc
 {
     class Functions
     {
-        public float add(float num1, float num2) => (float)Convert.ToDouble(num1 + num2);
-        public float sub(float num1, float num2) => (float)Convert.ToDouble(num1 - num2);
-        public float div(float num1, float num2) => (float)Convert.ToDouble(num1 / num2);
-        public float mul(float num1, float num2) => (float)Convert.ToDouble(num1 * num2);
+        private float num1, num2, result;
+        public float Num1 { get => num1; set => num1 = value; }
+        public float Num2 { get => num2; set => num2 = value; }
+        private float add() => (float)Convert.ToDouble(Num1 + Num2);
+        private float sub() => (float)Convert.ToDouble(Num1 - Num2);
+        private float div() => (float)Convert.ToDouble(Num1 / Num2);
+        private float mul() => (float)Convert.ToDouble(Num1 * Num2);
         public byte readNum(string text)
         {
             bool test = false;
@@ -28,55 +31,63 @@ namespace simpleCalc
             } while (test);
             return num;
         }
+        public void printResult(char operation)
+        {
+            float result = (operation == '+')
+                ? add()
+                    : (operation == '-')
+                ? sub()
+                    : (operation == '*')
+                ? mul()
+                    : div();
+            Console.WriteLine("Answer : {0}", result);
+
+
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             short select = 0;
-            float num1, num2, result;
             Functions operation = new Functions();
             do
             {
+                Console.Clear();
                 Console.WriteLine("\t--Calculator--\n");
                 Console.WriteLine("1. Addition");
                 Console.WriteLine("2. Subtraction");
                 Console.WriteLine("3. Multiplication");
                 Console.WriteLine("4. Division");
                 Console.WriteLine("5. Exit");
-                Console.Write("\nSelect Operation : ");
-                select = operation.readNum("");
-            } while (select < 1 || select > 6);
+                select = operation.readNum("\nSelect Operation : ");
+            } while (select < 1 || select > 5);
             Console.Clear();
             switch (select)
             {
                 case 1:
                     Console.WriteLine("\t--Addition--");
-                    num1 = operation.readNum("Enter num 1 : ");
-                    num2 = operation.readNum("Enter num 2 : ");
-                    result = operation.add(num1, num2);
-                    Console.WriteLine("Answer : {0}", result);
+                    operation.Num1 = operation.readNum("Enter num 1 : ");
+                    operation.Num2 = operation.readNum("Enter num 2 : ");
+                    operation.printResult('+');
                     break;
                 case 2:
                     Console.WriteLine("\t--Subtraction--");
-                    num1 = operation.readNum("Enter num 1 : ");
-                    num2 = operation.readNum("Enter num 2 : ");
-                    result = operation.sub(num1, num2);
-                    Console.WriteLine("Answer : {0}", result);
+                    operation.Num1 = operation.readNum("Enter num 1 : ");
+                    operation.Num2 = operation.readNum("Enter num 2 : ");
+                    operation.printResult('-');
                     break;
                 case 3:
                     Console.WriteLine("\t--Multiplication--");
-                    num1 = operation.readNum("Enter num 1 : ");
-                    num2 = operation.readNum("Enter num 2 : ");
-                    result = operation.mul(num1, num2);
-                    Console.WriteLine("Answer : {0}", result);
+                    operation.Num1 = operation.readNum("Enter num 1 : ");
+                    operation.Num2 = operation.readNum("Enter num 2 : ");
+                    operation.printResult('*');
                     break;
                 case 4:
                     Console.WriteLine("\t--Division--");
-                    num1 = operation.readNum("Enter num 1 : ");
-                    num2 = operation.readNum("Enter num 2 : ");
-                    result = operation.div(num1, num2);
-                    Console.WriteLine("Answer : {0}", result);
+                    operation.Num1 = operation.readNum("Enter num 1 : ");
+                    operation.Num2 = operation.readNum("Enter num 2 : ");
+                    operation.printResult('/');
                     break;
                 default: break;
             }
